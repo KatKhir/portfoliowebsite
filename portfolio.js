2dee48c8346722cbf9b914a317d0afb69ef2bd78
KatKhir <kasa0004@stud.kea.dk>
2025-06-09 00:08:52 +0200

    menu færdig

--- portfolio.js
+++ portfolio.js
@@ -6,3 +6,19 @@ window.addEventListener("scroll", () => {
   const rotation = scrollY % 360;
   image.style.transform = `rotate(${rotation}deg)`;
 });
+
+/******************************** MENU ***********************************/
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", burgerClick);
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+menu.addEventListener("click", menuClick);
+function menuClick() {
+  burger.classList.remove("active");
+  nav.classList.remove("active");
+}
